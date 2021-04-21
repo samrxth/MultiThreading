826be4db340d9fdbcdfc6cc6c88de1cb41042fe2
Samrath Chadha <69572668+samrath2007@users.noreply.github.com>
2021-04-21 21:44:16 +0530

    Added photo path + removed DS Store

--- download-image.py
+++ download-image.py
@@ -27,7 +27,7 @@ def download_image(img_url):
     img_bytes = requests.get(img_url).content
     img_name = img_url.split('/')[3]
     img_name = f'{img_name}.jpg'
-    with open(img_name, 'wb') as img_file:
+    with open('photos/' + img_name, 'wb') as img_file:
         img_file.write(img_bytes)
         print(f'{img_name} was downloaded...')
 
